fa918e408b2fa9ce79277c66c1a326a66f48ee90
Wilson Pulling <wilpulling@gmail.com>
2012-04-17 13:30:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SteppingCloudsBetaApp::Application.config.secret_token = '30ef728b662a4313d0cc8b54922e121864be4aef29d813c1e90bdffe5ded9ccb613a32a7ce016e54654629f5ae7c1aa1e39d5f410b4fd6a2c5cc8a7832ce5634'
